561b4d0b499ee2728ae61538b3778e45de9a3a2f
Brandow Buenos <brandowbuenos@gmail.com>
2021-09-21 17:55:25 -0300

    feat: add create dog route
    Co-authored-by: Willian Giovanni Pires Clemente <willianpires75@gmail.com>

--- src/routes/dogs.routes.ts
+++ src/routes/dogs.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { CreateDogController } from '../controllers'
 import { ListDogsController } from '../controllers/dogs/ListDogsController'
 
+const createDog = new CreateDogController()
 const listDogs = new ListDogsController()
 
 const dogsRoutes = (router: Router): void => {
+  router.post('/dogs', createDog.execute.bind(CreateDogController))
   router.get('/dogs', listDogs.execute.bind(ListDogsController))
 }
 
