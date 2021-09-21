447f15dc934765bc039f6f5df2f4247f6e2416a6
Brandow Buenos <brandowbuenos@gmail.com>
2021-09-21 17:19:26 -0300

    style: indent code

--- src/controllers/dogs/CreateDogController.ts
+++ src/controllers/dogs/CreateDogController.ts
@@ -1,44 +1,44 @@
-/* eslint-disable @typescript-eslint/space-before-function-paren */
-/* eslint-disable @typescript-eslint/indent */
 import { prisma } from '../../services'
 import { Request, Response } from 'express'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 class CreateDogController {
-    async execute(request: Request, response: Response): Promise<Response> {
-        try {
-            const { name, bornYear, wearCollar, genderId, ownerId, breedId } = request.body
+  async execute (request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, bornYear, wearCollar, genderId, ownerId, breedId } =
+        request.body
 
-            const owner = await prisma.owner.findFirst({
-                where: { id: ownerId }
-            })
-            if (!owner) {
-                throw new Error('Proprietário não existe')
-            }
+      const owner = await prisma.owner.findFirst({
+        where: { id: ownerId }
+      })
 
-            const createDog = await prisma.dog.create({
-                data: {
-                    id: uuidv4(),
-                    name: name,
-                    born_year: bornYear,
-                    wear_collar: wearCollar,
-                    is_dead: false,
-                    owner: { connect: { id: ownerId } },
-                    breed: { connect: { id: breedId } },
-                    gender: { connect: { id: genderId } }
-                }
-            })
+      if (!owner) {
+        throw new Error('Proprietário não existe')
+      }
 
-            return response.status(201).json({
-                body: createDog,
-                message: 'Cão cadastrado com sucesso'
-            })
-        } catch (error: any) {
-            return response.status(400).json({
-                message: error.message
-            })
+      const createDog = await prisma.dog.create({
+        data: {
+          id: uuidv4(),
+          name: name,
+          born_year: bornYear,
+          wear_collar: wearCollar,
+          is_dead: false,
+          owner_id: ownerId,
+          breed_id: breedId,
+          gender_id: genderId
         }
+      })
+
+      return response.status(201).json({
+        body: createDog,
+        message: 'Cão cadastrado com sucesso'
+      })
+    } catch (error: any) {
+      return response.status(400).json({
+        message: error.message
+      })
     }
+  }
 }
 
 export { CreateDogController }
