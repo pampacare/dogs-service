ade64a63a786e5c10ee0635d7c55030b37be954b
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-10-04 15:16:15 -0300

    fix: using heroku port

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ app.use(express.json())
 
 app.use(router)
 
-app.listen(3333, () =>
-  console.log('🚀 REST API server ready at: http://localhost:3333')
+app.listen(process.env.PORT ?? 3333, () =>
+  console.log('🚀 REST API server ready')
 )
