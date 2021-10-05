245c136efee14ee95632bf41b8766fc45423872a
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-10-05 18:04:29 -0300

    refactor: eslint

--- src/controllers/breed/ListBreedController.ts
+++ src/controllers/breed/ListBreedController.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from 'express'
 import {
-  prisma,
-  presetQueryFields,
-  convertToContains,
-  convertToNumber
+  prisma
 } from '../../services'
 
 class ListBreedController {
-  async execute(request: Request, response: Response): Promise<Response> {
+  async execute (request: Request, response: Response): Promise<Response> {
     try {
-      const breeds = await prisma.breed.findMany();
+      const breeds = await prisma.breed.findMany()
       return response.status(200).json(breeds)
     } catch (erro) {
       return response.status(400).json({
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from 'express'
 import { dogsRoutes } from './dogs.routes'
 import { ownerRoutes } from './owner.routes'
-import { breedRoutes} from './breed.routes'
+import { breedRoutes } from './breed.routes'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
