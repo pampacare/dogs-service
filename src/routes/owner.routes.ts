6633da545bd44c0f149c54fdbc5286dd6e40132b
Guilherme Samuel <guilhermesiqueira.aluno@unipampa.edu.br>
2021-10-05 16:54:17 -0300

    feat: list owners

--- src/routes/owner.routes.ts
+++ src/routes/owner.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { UpdateAddressOwner } from '../controllers/owners/UpdateAddresOwner'
 import CreateOwnerController from '../controllers/owners/CreateOwnerController'
+import { ListOwnersController } from '../controllers'
 
 const createOwnerController = new CreateOwnerController()
 const updateAddressOwner = new UpdateAddressOwner()
+const listOwners = new ListOwnersController()
 
 const ownerRoutes = (router: Router): void => {
   router.post('/owner', createOwnerController.execute.bind(CreateOwnerController))
   router.put('/owner/:id', updateAddressOwner.execute.bind(UpdateAddressOwner))
+  router.get('/owner', listOwners.execute.bind(ListOwnersController))
 }
 
 export { ownerRoutes }
